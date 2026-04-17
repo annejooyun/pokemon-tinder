6d37260839564b5dac3e13c2e5c12f6ac8ea33ff
Anne Joo Skogseth <ajskogseth@gmail.com>
2026-04-17 20:59:03 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# pokemon-tinder
+# Pokemon Tinder
+
+A React + TypeScript + Vite app for swiping through Pokemon.
+
+## Setup
+
+```bash
+npm install
+npm run dev
+```
+
+## Build
+
+```bash
+npm run build
+npm run preview
+```
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Pokemon Tinder</h1>
+      <div className="card-container">
+        {/* Pokemon cards will go here */}
+      </div>
+    </div>
+  )
+}
+
+export default App
